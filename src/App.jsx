69bb11290c251b81e4362c0d3bed24fc976fd0c3
BbillahrariBBr <mb.billah8@gmail.com>
2024-05-30 22:32:52 +0600

    done lifting the state up

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+/**
+ *
+ *
+ * <Game-->
+ * --> Board
+ *    --> Square
+ * --> History
+ */
+
+import { useState } from "react";
+
+function Square({ value }) {
+  function handleClick() {}
+  return (
+    <button
+      className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  return (
+    <>
+      <div className="flex">
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
+      </div>
+
+      <div className="flex">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
+      </div>
+
+      <div className="flex">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
+      </div>
+    </>
+  );
+}
